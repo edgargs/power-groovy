88b76b56b319b42969462e493ca2a7b827923c1d
Rios Navarro <edgar.riosnavarro@externos-pe.cencosud.com>
2018-02-13 17:57:08 -0500

    Se agrega pojo

--- src/main/groovy/com/gs/demo/GreetingController.groovy
+++ src/main/groovy/com/gs/demo/GreetingController.groovy
@@ -15,4 +15,54 @@ public class GreetingController {
 		return String.format(template, name);
 	}
 	
+	@RequestMapping("/greetingPerson")
+	public String greetingPerson(@RequestParam(value="name", defaultValue="World")String name) {
+		Person myPerson = new Person(name);
+		myPerson.setAge(35);		
+		return myPerson.sayHello();
+	}
+	
+}
+
+public class Person {
+
+	private String name;
+	private int age;
+	
+	public Person() {		
+	}
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String sayHello() {
+		String message = "";
+		message = message + "Hello,\n";
+		message = message + "My name is: " + this.name +"\n";
+		message = message + "and have "+ this.age + " years old.";
+		System.out.println(message);
+		return message;
+	}
 }
