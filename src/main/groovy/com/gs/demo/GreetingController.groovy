003560e7e44ec1784566d0a60d3a73d564f8da38
Rios Navarro <edgar.riosnavarro@externos-pe.cencosud.com>
2018-02-27 22:29:59 -0500

    final diff

--- src/main/groovy/com/gs/demo/GreetingController.groovy
+++ src/main/groovy/com/gs/demo/GreetingController.groovy
@@ -1,92 +1,54 @@
-package com.gs.demo;
+package com.gs.demo
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-public class GreetingController {
-
-	private static final String template = "Hello, %s!";
+class GreetingController {
 	
 	// http://localhost:8080/hello?name=Edgar
 	@RequestMapping("/hello")
-	public String hello(@RequestParam(value="name", defaultValue="World")String name) {
-		return String.format(template, name);
+	def hello(String name) {
+		"Hello, ${name?:'World'}!"
 	}
 	
 	@RequestMapping("/greeting")
-	public String greeting(@RequestParam(value="name", defaultValue="World")String name) {
-		Person myPerson = new Person(name);
-		myPerson.setAge(35);
+	def greeting(@RequestParam(value="name", defaultValue="World")def name) {
+		def myPerson = new Person(name:name, age:35)
 		
-		Map<String, Double> mapCryptoCurrency = new HashMap<>();
-		mapCryptoCurrency.put("XPR",1.07);
-		mapCryptoCurrency.put("BTC",2.09);
-		String virtual = "The price for \"BTC\" is " + mapCryptoCurrency.get("BTC");
+		Map<String, Double> mapCryptoCurrency = new HashMap<>()
+		mapCryptoCurrency.put "XPR",1.07
+		mapCryptoCurrency.put "BTC",2.09
+		def virtual = 'The price for "BTC" is ' + mapCryptoCurrency.get("BTC")
+		println virtual
 		
-		ArrayList<String> mylist = new ArrayList<>();
-		mylist.add("Grace");
-		mylist.add("Rob");
-		mylist.add("Emmy");
+		def mylist = ["Grace", "Rob", "Emmy"]
 		
-		String myname = myPerson.getName();
-		int myage = myPerson.getAge();
+		def (myname, myage) = [myPerson.name, myPerson.age]
 		
-		String message = null;
-		if(mylist.contains(myname)) {
-			message = "Welcome come back, " + myname + "!";
+		def message = null
+		if(myname in mylist) {
+			message = "Welcome come back, $myname!"
 		} else if(myage > 18) {
-			message = myPerson.sayHello();
+			message = myPerson.sayHello()
 		}
-		return message;
+		message
 	}
 	
 }
 
 class Person {
 
-	private String name;
-	private int age;
+	private def name
+	private int age
 	
-	public Person() {		
-	}
-	
-	public Person(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-	
-	public Person(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getAge() {
-		return this.age;
-	}
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	public String sayHello() {
-		String message = "";
-		message = message + "Hello,\n";
-		message = message + "My name is: " + this.name +"\n";
-		message = message + "and have "+ this.age + " years old.";
-		System.out.println(message);
-		return message;
+	def sayHello() {
+		def message = """
+		Hello,
+		My name is: $name
+		and have $age years old."""
+		println message
+		message
 	}
 }
